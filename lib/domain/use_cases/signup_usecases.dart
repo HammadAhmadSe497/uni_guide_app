9914b662333f75b76ba673847d8adb74b706a5fa
HammadAhmadSe497 <hammadahmadjutt497@gmail.com>
2024-08-05 15:03:33 +0500

    signUp and signIn page done with presentation and firebase integration

--- lib/domain/use_cases/signup_usecases.dart
+++ lib/domain/use_cases/signup_usecases.dart
@@ -1,13 +1,13 @@
-import '../entities/user.dart';
-import '../repositiories/auth_repo.dart';
-
-
-class SignUpUseCase {
-  final AuthRepository repository;
-
-  SignUpUseCase(this.repository);
-
-  Future<User?> call(String email, String password) {
-    return repository.signUp(email, password);
-  }
-}
+// import '../entities/user.dart';
+// import '../repositiories/auth_repo.dart';
+//
+//
+// class SignUpUseCase {
+//   final AuthRepository repository;
+//
+//   SignUpUseCase(this.repository);
+//
+//   Future<User?> call(String email, String password) {
+//     return repository.signUp(email, password);
+//   }
+// }
